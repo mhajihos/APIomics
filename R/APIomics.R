8f558e86716e4d53554b41a7c418965e3de79106
Morteza Hajihosseini <mhajihos@ualberta.ca>
2025-02-10 12:10:07 -0700

    commit

--- R/APIomics.R
+++ R/APIomics.R
@@ -431,6 +431,8 @@ ui <- dashboardPage(
                       style = "font-size: 14px; "),
                     p("Gene Disease Network: In this section, you can call the result from DEG Analysis, Master Regulators, and WGCNA to find the connections for various diseases using the DISGENET database. \nDisGeNET is a discovery platform for the dynamical exploration of human diseases and their genes",
                       style = "font-size: 14px; "),
+                    p("The gray boxes are under development.",
+                      style = "font-size: 16px; font-weight: bold;"),
                     tags$img(src = "static/flowchart.jpg", height = "50%", width = "70%")
                     
                     
