3fd1f330d7d4c5fc264ab7053a0e9cb97aac9c5a
Morteza Hajihosseini <mhajihos@ualberta.ca>
2025-02-10 09:31:16 -0700

    commit

--- R/APIomics.R
+++ R/APIomics.R
@@ -38,7 +38,7 @@ APIomics<-function()
 allowWGCNAThreads() 
 
 
-addResourcePath("static", system.file("www", package = "APIomics"))
+addResourcePath("static", "/path/to/APIomics/www")
 
 # 1000 MB (1 GB) file size limit
 options(shiny.maxRequestSize = 1000 * 1024^2) 
